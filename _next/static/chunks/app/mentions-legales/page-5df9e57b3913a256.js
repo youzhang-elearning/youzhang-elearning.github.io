af5e2286ba7b5493065ac0a1cf9008d3b498b97b
You Zhang <you.zhang@edufactory.com>
2026-09-08 11:19:29 +0200

    Publish updated estimation forms and multilingual adaptation workflow

---
+++ _next/static/chunks/app/mentions-legales/page-5df9e57b3913a256.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[640,648],{2552:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8500,23))}},e=>{e.O(0,[500,441,794,358],()=>e(e.s=2552)),_N_E=e.O()}]);
